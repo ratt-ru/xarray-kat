f7af73238ccbafc461dba48f5d73579049113ba8
Simon Perkins <simon.perkins@gmail.com>
2025-11-25 09:36:18 +0200

    comments

--- src/xarray_kat/katdal_types.py
+++ src/xarray_kat/katdal_types.py
@@ -7,4 +7,8 @@ from xarray_kat.third_party.vendored.katdal.visdatav4_minimal import VisibilityD
 
 
 def sensor_cache_factory(datasource: Multiton[TelstateDataSource]) -> SensorCache:
+  """ katdal Dataset code generates the sensor cache.
+
+  Call the constructor and appropriate the sensors.
+  """
   return VisibilityDataV4(datasource.instance).sensor
